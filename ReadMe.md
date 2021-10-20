517cea669f608d546ed589197cafb1ec489c3407
SAHEED <refactormyself@gmail.com>
2021-10-20 04:33:00 +0100

    Add Sample MAHI GUI

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+This is a demo of [MAHI-GUI framework](https://github.com/mahilab/mahi-gui).
+
+It should work with running the `./run.sh` script
--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
 
+# install gui dependencies
+sudo apt-get install build-essential libgtk-3-dev
+sudo apt-get install libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
+sudo apt-get install --reinstall pkg-config cmake-data
+
 CURR_DIR=$PWD
+rf -rf "build"
 mkdir "build"
 cd build || exit
 
 cmake ..
 make
-make test
+# make test
 
-./cli_app_basic
+./app 
 ./test/common_tests
 
 cd $CURR_DIR
